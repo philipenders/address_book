fc00bf671ac5d4b015f2301155ba2e00cdbacbe3
Philip <philip.enders1@gmail.com>
2017-12-18 15:00:51 -0500

    more tweaks added get address from name function

--- Book reader.py
+++ Book reader.py
@@ -58,7 +58,20 @@ class Book:
         # adds the Entry object to the currently active Book.
         self.data.append(entry_to_add)
 
-#The entry class objects make up Book objects and contain a parsed version of the data from the csv file.
+    def address_from_full_name(self):
+        search_term = input("Please Enter the Full Name of the desired Contact")
+        found = True
+        for datapoint in self.data:
+            found = False
+            if datapoint.Full_Name == search_term:
+                print datapoint.Full_Address
+                found = True
+                break
+        if found == False:
+            print("error, no such name found. Returning to Main Menu.")
+
+
+# The entry class objects make up Book objects and contain a parsed version of the data from the csv file.
 class Entry:
     def __init__(self, data_line):
         self.Raw_Entry = data_line
@@ -70,16 +83,17 @@ class Entry:
         self.Zip = data_line[5]
         self.Phone_Number = data_line[6]
         self.Email = data_line[7]
-        #below this point, I combine the above parsed data into slightly more consumable peices.
+        # below this point, I combine the above parsed data into slightly more consumable peices.
         self.Full_Address = self.Street_Address + " " + self.City + ", " + self.State + " " + self.Zip
         self.Full_Name = self.First_Name + " " + self.Last_Name
         self.Contact_Info = [self.Phone_Number, self.Email]
 
-#it's not the most beautiful but it is
+    # it's not the most beautiful but it is
     def __repr__(self):
         return str(self.Raw_Entry)
 
-#I have this outside of the body of the program for testing purposes.
+
+# I have this outside of the body of the program for testing purposes.
 def mainloop():
     invalid_entry_count = 0
     while True:
@@ -89,7 +103,8 @@ def mainloop():
         print("Please select an option.")
         print("1. list contacts and contact information.")
         print("2. add a contact to the directory.")
-        print("3. Exit")
+        print("3. find an address given a full name.")
+        print("4. Exit")
 
         choice = raw_input("Please enter a number corresponding to your choice: ")
         if choice == '1':
@@ -100,8 +115,10 @@ def mainloop():
             mybook.add_contact()
             mybook.update_book()
             print("Directory updated")
-
         elif choice == '3':
+            mybook.address_from_full_name()
+
+        elif choice == '4':
             print("Quitting...")
             break
         else:
@@ -113,5 +130,7 @@ def mainloop():
 
 
 mybook = Book()
+print("Thanks for taking a look at this project. It's very simple right now. Very basic UI.")
+print("The idea is to create an updatable book of contacts in a csv file.")
 
 mainloop()
